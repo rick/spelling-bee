d59c82f1371301bd9cf3f0112d202fb9596d2013
Rick Bradley <rick@rickbradley.com>
2022-12-07 16:53:59 -0600

    Remove original bash PoC definition lookup script

--- script/lookup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-sed 's,^\(.*\)$,curl -s -H "Accept: application/json" -H "Content-Type: application/json" -o - "https://www.dictionaryapi.com/api/v3/references/sd3/json/\1?key='${API_KEY}'",' | \
-	 sh -s -x | \
-	 jq '.[].def[].sseq[][][1].dt[0][1]' | \
-	 sed 's,{[^\{]*},,g' | \
-	 grep '[a-zA-Z]' | \
-	 sed 's,^,    ,'
