b1c544dd6853f25a1b048038423f000f11c25696
Rick Bradley <rick@rickbradley.com>
2022-12-07 15:00:17 -0600

    Document trivial word cleaner script

--- script/clean.sh
+++ script/clean.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+#
+# given a list of words on STDIN (from our original PDF of 2022 spelling bee words)
+# remove all `*` and `OR` (for alternative spellings) and sort the list,
+# then output the list to STDOUT
+#
+#
+# Usage: ./clean.sh < spelling-bee-words.txt > spelling-bee-words-clean.txt
+
 grep '[a-zA-z]' | sed 's,\*,,g' | sed 's,^OR ,,' | sort
