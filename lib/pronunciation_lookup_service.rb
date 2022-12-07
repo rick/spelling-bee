29c248a515bd516e41b09f133caf284a38641570
Rick Bradley <rick@rickbradley.com>
2022-12-07 17:29:21 -0600

    Add a pronunciation lookup service

---
+++ lib/pronunciation_lookup_service.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'cgi'
+require 'dotenv/load'
+require 'down'
+require 'http'
+
+# Wrap Google static pronuncation files endpoint, providing a simple interface for fetching word pronunciations
+class PronunciationLookupService
+  attr_reader :url
+
+  def initialize
+    @url = 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/%%%WORD%%%--_us_1.mp3'
+  end
+
+  def pronunciation_audio_for(word)
+    full_url = url.gsub('%%%WORD%%%', CGI.escape(word))
+    Down.download(full_url)
+  rescue Down::Error => err
+    STDERR.puts "Failed to download pronunciation for #{word}: #{err}"
+    false
+  end
+end
